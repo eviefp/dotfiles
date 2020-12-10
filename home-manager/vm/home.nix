baf8ada998247951bd47cf66db165791b1e8700f
Vladimir Ciobanu <admin@cvlad.info>
2020-12-10 15:25:02 +0200

    Update vm home files.

--- home-manager/vm/home.nix
+++ home-manager/vm/home.nix
@@ -22,7 +22,15 @@ in
     ++ common.packages.streaming;
 
   home.sessionVariables = common.sessionVariables;
-  home.file = common.file // emacs.file;
+  home.file = common.file // emacs.file // {
+      ".xmobarrc".source = ../../config/carbon/xmobarrc;
+      ".xmonad/xmonad.hs".source = ../../config/xmonad/xmonad.hs;
+      ".xmonad/get-mails.sh".source = ../../config/xmonad/get-mails.sh;
+      ".xmonad/get-mic.sh".source = ../../config/xmonad/get-mic.sh;
+      ".xmonad/get-volume.sh".source = ../../config/xmonad/get-volume.sh;
+      ".config/fish/functions/fixUI.fish".source = ../../config/fish/functions/fixUI.fish;
+      ".config/fish/functions/ssh.fish".source = ../../config/fish/functions/ssh.fish;
+    };
 
   programs = common.programs;
 
