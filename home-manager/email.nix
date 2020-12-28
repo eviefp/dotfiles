6563aa6710e252dab31ecb04abfb9c23cf0d932e
Evie Ciobanu <me@evie.ro>
2020-12-29 01:28:37 +0200

    testing muchsync

--- home-manager/email.nix
+++ home-manager/email.nix
@@ -1,31 +1,37 @@
-{ pkgs, ... }:
+{ pkgs, asServer ? true }:
 let
-  pass = "${pkgs.pass}/bin/pass";
   gmail = {
     primary = true;
     address = "vlad@cvlad.info";
     flavor = "gmail.com";
     msmtp.enable = true;
-    mbsync = {
-      enable = true;
-      create = "both";
-      expunge = "both";
-      remove = "none";
-      patterns = [
-        "INBOX"
-        "\\[Gmail\\]/All Mail"
-        "\\[Gmail\\]/Drafts"
-        "\\[Gmail\\]/Spam"
-        "\\[Gmail\\]/Sent Mail"
-      ];
-    };
+    mbsync =
+      if asServer
+        then
+            {
+                enable = true;
+                create = "both";
+                expunge = "both";
+                remove = "none";
+                patterns = [
+                    "INBOX"
+                    "\\[Gmail\\]/All Mail"
+                    "\\[Gmail\\]/Drafts"
+                    "\\[Gmail\\]/Spam"
+                    "\\[Gmail\\]/Sent Mail"
+                ];
+            }
+      else { enable = false; };
     notmuch.enable = true;
     smtp = {
       host = "smtp.gmail.com";
       tls.useStartTls = true;
       port = 587;
     };
-    imap.host = "imap.gmail.com";
+    imap.host =
+      if asServer
+        then "imap.gmail.com"
+        else null;
     realName = "Vladimir Ciobanu";
     userName = "admin@cvlad.info";
     passwordCommand = "cat /home/vlad/.secrets/cvlad.pwd";
