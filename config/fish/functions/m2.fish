cd726a3f47bf8d57cf5e4b274ee1c97331129af1
Evie Ciobanu <me@evie.ro>
2021-01-17 13:30:21 +0200

    asdf

--- config/fish/functions/m2.fish
+++ config/fish/functions/m2.fish
@@ -1,6 +1,5 @@
 function m2 -d "2 monitor setup"
-    xrandr --output DP2 --mode 1920x1080 --primary --left-of eDP1
-    xrandr --output HDMI1 --mode 1920x1080 --left-of DP2
-    xrandr --output eDP1 --off
+    xrandr --output HDMI-0 --mode 1920x1080 --primary
+    xrandr --output DP-2 --mode 1920x1080 --right-of HDMI-0
     fixUI
 end
