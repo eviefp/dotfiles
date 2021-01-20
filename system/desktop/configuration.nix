935853beb663090b356a09b3de776d4b56222e28
Evie Ciobanu <me@evie.ro>
2021-01-21 01:37:43 +0200

    rename

--- system/desktop/configuration.nix
+++ system/desktop/configuration.nix
@@ -14,7 +14,7 @@
   boot.loader.systemd-boot.enable = true;
   boot.loader.efi.canTouchEfiVariables = true;
 
-  networking.hostName = "desktop"; # Define your hostname.
+  networking.hostName = "arche"; # Define your hostname.
 
   # The global useDHCP flag is deprecated, therefore explicitly set to false here.
   # Per-interface useDHCP will be mandatory in the future, so this generated config
