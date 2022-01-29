bf85c7eaadc7b00e1c711d87636514e1047591dc
Evie Ciobanu <me@evie.ro>
2022-01-29 19:10:23 +0200

    wezterm

--- home-manager/modules/programs.nix
+++ home-manager/modules/programs.nix
@@ -18,6 +18,7 @@ in
     ./programs/shell/ranger.nix
     ./programs/streaming.nix
     ./programs/text.nix
+    ./programs/wezterm.nix
   ];
 
   options.evie.programs = {
---
+++ home-manager/modules/programs/wezterm.nix
@@ -0,0 +1,36 @@
+/****************************************************************************
+  * programs/wezterm module
+  *
+  * https://wezfurlong.org/wezterm
+  *
+  **************************************************************************/
+{ lib, config, pkgs, ... }:
+let cfg = config.evie.programs.wezterm;
+in
+{
+  imports = [ ];
+
+  options.evie.programs.wezterm = {
+    enable = lib.options.mkEnableOption "Enable wezterm";
+  };
+
+  config = lib.mkIf cfg.enable {
+    home.packages = [
+      pkgs.wezterm
+    ];
+    home.file.".config/wezterm/wezterm.lua".text = ''
+local wezterm = require 'wezterm'
+
+return {
+  default_prog = {"${pkgs.fish}/bin/fish"},
+  -- launch_menu = {},
+  font = wezterm.font('Hasklug Nerd Font Mono', { weight = "Regular", stretch = "Normal", italic = false }),
+  check_for_updates = false,
+  font_size = 10,
+  color_scheme = 'Molokai',
+  enable_tab_bar = false,
+  window_background_opacity = 0.7,
+}
+'';
+  };
+}
