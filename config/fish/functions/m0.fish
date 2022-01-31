6f3e8228756ef569b4e5b1cebdf44380a6683356
Evie Ciobanu <me@evie.ro>
2022-01-31 15:50:54 +0200

    random fixes

--- config/fish/functions/m0.fish
+++ config/fish/functions/m0.fish
@@ -1,5 +1,5 @@
 function m0 -d "main monitor setup"
-    xrandr --output HDMI1 --off
+    xrandr --output HDMI-1 --off
     xrandr --output DP-0 --mode 1920x1080 --rate 239.76 --left-of DP-2
     xrandr --output DP-2 --mode 1920x1080 --rate 239.76 --primary
     fixUI
