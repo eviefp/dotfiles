f81ecf3e66ec5f6cc812d94e2d7c6d98d75c46e6
Evie Ciobanu <me@eevie.ro>
2022-02-04 18:10:15 +0200

    replace theme

--- config/nvim/lua/plugins/lualine.lua
+++ config/nvim/lua/plugins/lualine.lua
@@ -7,7 +7,7 @@ local lualine = require 'lualine'
 lualine.setup {
   options = {
     icons_enabled = true,
-    theme = 'gruvbox',
+    theme = 'catppuccin',
     component_separators = { left = '', right = ''},
     section_separators = { left = '', right = ''},
     disabled_filetypes = {},
