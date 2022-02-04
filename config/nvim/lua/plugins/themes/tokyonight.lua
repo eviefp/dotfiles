92f824029dc00829a2543757ae40478088400663
Evie Ciobanu <me@eevie.ro>
2022-02-04 21:43:42 +0200

    themes, lightspeed

---
+++ config/nvim/lua/plugins/themes/tokyonight.lua
@@ -0,0 +1,7 @@
+-------------------------------------------------------------------------------
+-- tokyonight
+-------------------------------------------------------------------------------
+
+vim.g.tokyonight_style = 'night'
+vim.g.tokyonight_transparent = true
+vim.g.tokyonight_transparent_sidebar = true
