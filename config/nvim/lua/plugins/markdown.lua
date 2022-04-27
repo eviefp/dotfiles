8824cf3fc823159a95b45f0bb7de1a0fc66b6276
Evie Ciobanu <me@eevie.ro>
2022-04-27 17:59:45 +0300

    markdown

--- config/nvim/lua/plugins/markdown.lua
+++ config/nvim/lua/plugins/markdown.lua
@@ -2,6 +2,14 @@
 -- markdown
 -------------------------------------------------------------------------------
 
-vim.g.markdown_fenced_languages = { 'html', 'python', 'bash=sh', 'nix', 'haskell', 'purescript' }
-vim.g.markdown_minlines = 256
+vim.w.conceallevel = 2
+vim.wo.conceallevel = 2
 
+vim.g.vim_markdown_folding_disabled = 0
+vim.g.vim_markdown_folding_level = 0
+vim.g.vim_markdown_toc_autofit = 1
+vim.g.vim_markdown_fenced_languages = { 'html', 'python', 'bash=sh', 'nix', 'haskell', 'purescript' }
+vim.g.vim_markdown_follow_anchor = 1
+vim.g.vim_markdown_markdown_math = 1
+vim.g.vim_markdown_frontmatter = 1
+vim.g.vim_markdown_strikethrough = 1
