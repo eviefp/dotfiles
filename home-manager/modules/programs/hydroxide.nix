b46c407b23f8f8c115ea90c162a7d205312b4fbc
Evie Ciobanu <me@eevie.ro>
2022-04-27 12:15:11 +0300

    fix email stuff

--- home-manager/modules/programs/hydroxide.nix
+++ home-manager/modules/programs/hydroxide.nix
@@ -5,18 +5,37 @@
 { lib, config, pkgs, ... }:
 let
   cfg = config.evie.programs.hydroxide;
-  package = pkgs.callPackage ./hydroxide-package.nix {};
+  package = pkgs.callPackage ./hydroxide-package.nix { };
 in
 {
   imports = [ ];
 
   options.evie.programs.hydroxide = {
     enable = lib.options.mkEnableOption "Enable hydroxide";
+    service = lib.options.mkEnableOption "Enable hydroxide service";
   };
 
   config = lib.mkIf cfg.enable {
     home.packages = [
       package
     ];
+
+    systemd.user.services.hydroxide = lib.mkIf cfg.service {
+      Unit = {
+        Description = "Hydroxide Protonmail Bridge";
+      };
+
+      Install = { WantedBy = [ "default.target" ]; };
+
+      Service = {
+        Type = "forking";
+
+        ExecStart = "${package}/bin/hydroxide serve";
+
+        Restart = "always";
+
+        RestartSec = 3;
+      };
+    };
   };
 }
