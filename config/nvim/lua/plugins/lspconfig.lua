693f4cba2dddb410d7cf9e26f3fb6851b9d06ee5
Evie Ciobanu <me@evie.ro>
2022-11-25 11:10:43 +0100

    update and fix lspconfig deprecation

--- config/nvim/lua/plugins/lspconfig.lua
+++ config/nvim/lua/plugins/lspconfig.lua
@@ -6,7 +6,7 @@ local lspconfig = require 'lspconfig'
 
 -- Add additional capabilities supported by nvim-cmp
 local capabilities = vim.lsp.protocol.make_client_capabilities()
-capabilities = require('cmp_nvim_lsp').update_capabilities(capabilities)
+capabilities = require('cmp_nvim_lsp').default_capabilities(capabilities)
 
 -------------------------------------------------------------------------------
 -- Haskell
