7b4c36de6c6a4b1704542a695acbd980d98106a4
Evie Ciobanu <me@eevie.ro>
2023-10-03 22:02:35 +0300

    flakify #1

---
+++ flake.nix
@@ -0,0 +1,25 @@
+{
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-23.05";
+    home-manager.url = "github:nix-community/home-manager";
+    home-manager.inputs.nixpkgs.follows = "nixpkgs";
+  };
+
+  outputs = { self, nixpkgs, home-manager }: {
+
+    nixosConfigurations."thelxinoe" = nixpkgs.lib.nixosSystem {
+      system = "x86_64-linux";
+      modules =
+        [
+          ./system/thelxinoe/configuration.nix
+          # home-manager.nixosModules.home-manager
+          # {
+          #   home-manager.useGlobalPkgs = true;
+          #   home-manager.useUserPackages = true;
+          #   home-manager.users.evie = import ./home-manager/thelxinoe/home.nix;
+          # }
+        ];
+    };
+
+  };
+}
