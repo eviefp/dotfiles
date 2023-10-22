02e19fc62630e46b1aac8cf14685e294e4e27670
Evie Ciobanu <me@evie.ro>
2023-10-22 18:25:34 +0300

    fix

--- flake.nix
+++ flake.nix
@@ -389,7 +389,6 @@
           home-manager.nixosModules.home-manager
           {
             home-manager.useGlobalPkgs = true;
-            home-manager.useUserPackages = true;
             home-manager.extraSpecialArgs = {
               inherit nvim-visuals-multi nvim-bbye nvim-kommentary
                 nvim-tabular nvim-rainbow nvim-abolish nvim-fugitive
