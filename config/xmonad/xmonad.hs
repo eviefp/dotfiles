46b91787cdb9d42e6d2249829e7a3e2fb217cc53
Evie Ciobanu <me@eevie.ro>
2023-10-26 22:48:20 +0300

    email fixes, remove neuron, minor xmonad update

--- config/xmonad/xmonad.hs
+++ config/xmonad/xmonad.hs
@@ -325,7 +325,7 @@ allKeys =
     xrandr =
       [ ("M-a", Xrandr "xrandr --output HDMI-1 --mode 1920x1080 --right-of DP-4"),
         ("M-z", Xrandr "xrandr --output HDMI-1 --off"),
-        ("M-x", Xrandr "xrandr --output DP-0 --mode 1920x1080 --rate 239.76 --left-of DP-2 --primary; xrandr --output DP-2 --mode 1920x1080 --rate 239.76")
+        ("M-x", Xrandr "xrandr --output DP-0 --mode 1920x1080 --rate 239.76 --left-of DP-2; xrandr --output DP-2 --mode 1920x1080 --rate 239.76 --primary; xrandr --output DP-4 --mode 1920x1080 --rate 239.76 --right-of DP-2")
       ]
 
     audio :: [(String, KeyAction)]
