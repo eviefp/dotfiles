673ff5e574de355f3638e4e476552bdbdec71d27
Evie Ciobanu <me@evie.ro>
2023-12-30 02:33:09 +0200

    toggle tv, show next calendar entry in eww

---
+++ config/eww/scripts/toggle-tv.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+
+num_monitors=`hyprctl monitors -j | jq length`
+
+if [[ "$num_monitors" -lt "4" ]]; then
+  hyprctl keyword monitor HDMI-A-2,1920x1080@60,5760x0,1
+else
+  hyprctl keyword monitor HDMI-A-2,disable
+fi
