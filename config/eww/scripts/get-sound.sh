4c2c3a539159e4672409b463b3a8042b133db808
Evie Ciobanu <me@evie.ro>
2023-12-31 02:43:44 +0200

    update icons, microphone, hyprpicker

--- config/eww/scripts/get-sound.sh
+++ config/eww/scripts/get-sound.sh
@@ -1,17 +1,35 @@
 #!/usr/bin/env bash
 
-status=`amixer get Master | tail -n1 | grep -wo 'on'`
+sound=`amixer get Master | tail -n1 | grep -wo 'on'`
 
-if [[ "$status" == "on" ]]; then
-  volume=`amixer get Master | tail -n1 | awk -F ' ' '{print $5}' | tr -d '[]%'`
+if [[ "$sound" == "on" ]]; then
+  svolume=`amixer get Master | tail -n1 | awk -F ' ' '{print $5}' | tr -d '[]%'`
 else
-  volume="0"
+  svolume="0"
 fi
 
-if [[ "$volume" -gt "0" ]]; then
-    icon="./assets/volume.png"
+if [[ "$svolume" -gt "0" ]]; then
+    sicon="./assets/volume.png"
 else
-    icon="./assets/mute.png"
+    sicon="./assets/mute.png"
 fi
 
-echo "{\"volume\": \"$volume\", \"icon\": \"$icon\"}"
+microphone=`amixer get Capture | tail -n1 | grep -wo 'on'`
+
+if [[ "$microphone" == "on" ]]; then
+  mvolume=`amixer get Capture | tail -n1 | awk -F ' ' '{print $4}' | tr -d '[]%'`
+else
+  mvolume="0"
+fi
+
+if [[ "$mvolume" -gt "0" ]]; then
+    micon="./assets/mic-volume.png"
+else
+    micon="./assets/mic-mute.png"
+fi
+
+echo \
+    "{\"svolume\": \"$svolume\",\
+      \"sicon\": \"$sicon\",\
+      \"mvolume\": \"$mvolume\",\
+      \"micon\": \"$micon\"}"
