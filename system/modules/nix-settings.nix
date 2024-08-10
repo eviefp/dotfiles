a70a5dc223e9bed668b63f6995af282fe7a97fe1
Evie Ciobanu <me@evie.ro>
2024-08-10 14:05:33 +0300

    substituters

--- system/modules/nix-settings.nix
+++ system/modules/nix-settings.nix
@@ -11,12 +11,14 @@
     nix = {
       settings = {
         trusted-users = [ "root" "evie" ];
-        extra-substituters = [
+        trusted-substituters = [
           "https://cache.lix.systems"
+          "https://cache.garnix.io"
         ];
 
         trusted-public-keys = [
           "cache.lix.systems:aBnZUw8zA7H35Cz2RyKFVs3H4PlGTLawyY5KRbvJR8o="
+          "cache.garnix.io:CTFPyKSLcx5RMJKfLo5EEPUObbA78b0YQ2DTCJXqr9g="
         ];
       };
 
