9b6ef1cd579db0062734521863ab3c6b1ca635fc
Evie Ciobanu <me@evie.ro>
2024-11-27 22:26:27 +0200

    eww-weather: try fixing the script, move secret to sops

--- config/eww/scripts/weather.sh
+++ config/eww/scripts/weather.sh
@@ -1,11 +1,13 @@
 #!/usr/bin/env bash
 
-apikey=`cat ~/.secrets/weather.key`
+apikey=`cat /var/run/secrets/weatherKey`
 
 weather=`http 'http://api.weatherapi.com/v1/current.json?key='$apikey'&q=Bucharest&aqi=yes'`
 
 icon=`echo $weather | jq -r .current.condition.icon`
 
 http http:$icon -o /home/evie/.config/eww-extras/weather.png
+
+date > /home/evie/.config/eww-extras/last-run
 
 echo $weather
