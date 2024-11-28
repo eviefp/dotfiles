e771f7d3f092ad865473e915fb71f019724fbeb8
Evie Ciobanu <me@evie.ro>
2024-11-28 18:13:11 +0200

    eww: fix weather, again?

--- config/eww/scripts/weather.sh
+++ config/eww/scripts/weather.sh
@@ -6,8 +6,19 @@ weather=`http 'http://api.weatherapi.com/v1/current.json?key='$apikey'&q=Buchare
 
 icon=`echo $weather | jq -r .current.condition.icon`
 
-http http:$icon -o /home/evie/.config/eww-extras/weather.png
+http http:$icon -o /home/evie/.config/eww-extras/weather_tmp.png
 
-date > /home/evie/.config/eww-extras/last-run
+fileType=`file -i /home/evie/.config/eww-extras/weather_tmp.png`
+echo $fileType
+if [[ "$fileType" == "/home/evie/.config/eww-extras/weather_tmp.png: image/png; charset=binary" ]]; then
+    mv /home/evie/.config/eww-extras/weather_tmp.png /home/evie/.config/eww-extras/weather.png
+    echo "success" >> /home/evie/.config/eww-extras/last-run
+else
+  rm /home/evie/.config/eww-extras/weather_tmp.png
+fi
+
+
+date >> /home/evie/.config/eww-extras/last-run
+
 
 echo $weather
