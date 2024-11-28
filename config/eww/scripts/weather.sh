33d10796f406b47af39a9fca78b2c533b16af2a8
Evie Ciobanu <me@evie.ro>
2024-11-28 19:16:07 +0200

    weather.sh: add more debugging info

--- config/eww/scripts/weather.sh
+++ config/eww/scripts/weather.sh
@@ -8,17 +8,16 @@ icon=`echo $weather | jq -r .current.condition.icon`
 
 http http:$icon -o /home/evie/.config/eww-extras/weather_tmp.png
 
+now=`date`
 fileType=`file -i /home/evie/.config/eww-extras/weather_tmp.png`
 echo $fileType
 if [[ "$fileType" == "/home/evie/.config/eww-extras/weather_tmp.png: image/png; charset=binary" ]]; then
     mv /home/evie/.config/eww-extras/weather_tmp.png /home/evie/.config/eww-extras/weather.png
-    echo "success" >> /home/evie/.config/eww-extras/last-run
+    echo "$now success " >> /home/evie/.config/eww-extras/last-run
 else
+  echo "$now failure; weather: $weather" >> /home/evie/.config/eww-extras/last-run
+  echo "              icon   : $icon" >> /home/evie/.config/eww-extras/last-run
   rm /home/evie/.config/eww-extras/weather_tmp.png
 fi
-
-
-date >> /home/evie/.config/eww-extras/last-run
-
 
 echo $weather
