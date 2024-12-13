de2479d1ac9fca0a71874161ead6f049bf234f26
Evie Ciobanu <me@evie.ro>
2024-12-13 13:51:59 +0200

    spotify_player: move defaults to the module

--- modules/home-manager/term/spotify.nix
+++ modules/home-manager/term/spotify.nix
@@ -24,6 +24,156 @@ in
         Spotify-player configuration.
         For available settings, see <>.
       '';
+      default = {
+        actions = [
+          {
+            action = "AddToLiked";
+            key_sequence = "a l";
+          }
+          {
+            action = "AddToQueue";
+            key_sequence = "a q";
+          }
+          {
+            action = "DeleteFromLiked";
+            key_sequence = "d l";
+          }
+        ];
+        keymaps = [
+          {
+            command = "NextTrack";
+            key_sequence = "J";
+          }
+          {
+            command = "PreviousTrack";
+            key_sequence = "K";
+          }
+          {
+            command = "ResumePause";
+            key_sequence = "space";
+          }
+          {
+            command = "Repeat";
+            key_sequence = "C-r";
+          }
+          {
+            command = "Shuffle";
+            key_sequence = "C-s";
+          }
+          {
+            command = "VolumeUp";
+            key_sequence = "+";
+          }
+          {
+            command = "VolumeDown";
+            key_sequence = "-";
+          }
+          {
+            command = "SeekForward";
+            key_sequence = ">";
+          }
+          {
+            command = "SeekBackward";
+            key_sequence = "<";
+          }
+          {
+            command = "Quit";
+            key_sequence = "Q";
+          }
+          {
+            command = "ClosePopup";
+            key_sequence = "esc";
+          }
+          {
+            command = "SelectNextOrScrollDown";
+            key_sequence = "j";
+          }
+          {
+            command = "SelectPreviousOrScrollUp";
+            key_sequence = "k";
+          }
+          {
+            command = "SelectFirstOrScrollToTop";
+            key_sequence = "g g";
+          }
+          {
+            command = "SelectLastOrScrollToBottom";
+            key_sequence = "G";
+          }
+          {
+            command = "ChooseSelected";
+            key_sequence = "enter";
+          }
+          {
+            command = "RefreshPlayback";
+            key_sequence = "g r";
+          }
+          {
+            command = "ShowActionsOnSelectedItem";
+            key_sequence = "a s";
+          }
+          {
+            command = "ShowActionsOnCurrentTrack";
+            key_sequence = "a c";
+          }
+          {
+            command = "Search";
+            key_sequence = "/";
+          }
+          {
+            command = "BrowseUserPlaylists";
+            key_sequence = "f p";
+          }
+          {
+            command = "BrowseUserFollowedArtists";
+            key_sequence = "f a";
+          }
+          {
+            command = "BrowseUserSavedAlbums";
+            key_sequence = "f b";
+          }
+          {
+            command = "CurrentlyPlayingContextPage";
+            key_sequence = "f c";
+          }
+          {
+            command = "LyricPage";
+            key_sequence = "g v";
+          }
+          {
+            command = "LibraryPage";
+            key_sequence = "g l";
+          }
+          {
+            command = "SearchPage";
+            key_sequence = "g s";
+          }
+          {
+            command = "BrowsePage";
+            key_sequence = "g b";
+          }
+          {
+            command = "Queue";
+            key_sequence = "g q";
+          }
+          {
+            command = "OpenCommandHelp";
+            key_sequence = "?";
+          }
+          {
+            command = "MovePlaylistItemUp";
+            key_sequence = "C-k";
+          }
+          {
+            command = "MovePlaylistItemDown";
+            key_sequence = "C-j";
+          }
+          {
+            command = "CreatePlaylist";
+            key_sequence = "N";
+          }
+        ];
+      };
     };
     # theme = lib.mkOption {
     #   type = settingsFormat.type;
