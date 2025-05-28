b33126bf52a598fc643700d126d74a6d9e1fca28
Evie Ciobanu <me@evie.ro>
2025-05-28 03:16:36 +0300

    add lact and tectonic

--- modules/nixos/hardware/video.nix
+++ modules/nixos/hardware/video.nix
@@ -41,6 +41,9 @@ in
       };
     })
     (lib.mkIf cfg.amdgpu.enable {
+      environment.systemPackages = [ pkgs.lact ];
+      systemd.packages = [ pkgs.lact ];
+      systemd.services.lact.wantedBy = [ "multi-user.target" ];
       hardware.amdgpu = {
         initrd.enable = true;
         opencl.enable = true;
