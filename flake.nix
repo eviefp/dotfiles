be31c32a7814d818fc5a49858d2e94e654d1da6a
Evie Ciobanu <me@evie.ro>
2025-08-05 11:59:22 +0300

    cannot update

--- flake.nix
+++ flake.nix
@@ -1,5 +1,6 @@
 {
   inputs = {
+    # https://git.lix.systems/lix-project/lix/issues/943
     lix-module = {
       url = "https://git.lix.systems/lix-project/nixos-module/archive/2.93.3-1.tar.gz";
       inputs.nixpkgs.follows = "nixpkgs";
