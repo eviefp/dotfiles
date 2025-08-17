beb073e79beeae72d65b0d12edd899e0dbb593df
Evie Ciobanu <me@evie.ro>
2025-08-17 19:48:33 +0300

    monitors fix

--- config/eww/scripts/toggle-tv.sh
+++ config/eww/scripts/toggle-tv.sh
@@ -3,7 +3,7 @@
 num_monitors=`hyprctl monitors -j | jq length`
 
 if [ "$num_monitors" -lt "4" ]; then
-  hyprctl keyword monitor "HDMI-A-1,1920x1080@60,5760x0,1"
+  hyprctl keyword monitor "HDMI-A-1,1920x1080@60,0x395,1"
 else
   hyprctl keyword monitor "HDMI-A-1,disabled"
 fi
