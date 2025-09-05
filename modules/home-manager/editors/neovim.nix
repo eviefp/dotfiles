e87535b7786f59243eeb1f1f9dc0472505edcc99
Evie Ciobanu <me@evie.ro>
2025-09-05 23:13:09 +0300

    neovim: find and replace

--- modules/home-manager/editors/neovim.nix
+++ modules/home-manager/editors/neovim.nix
@@ -1067,6 +1067,25 @@ in
           };
         };
 
+        # Find-and-Replace UI.
+        grug-far = {
+          enable = true;
+          settings = {
+            debounceMs = 1000;
+            minSearchChars = 1;
+            maxSearchMatches = 2000;
+            normalModeSearch = false;
+            maxWorkers = 8;
+            engine = "ripgrep";
+            engines = {
+              ripgrep = {
+                path = "rg";
+                showReplaceDiff = true;
+              };
+            };
+          };
+        };
+
       };
     };
   };
