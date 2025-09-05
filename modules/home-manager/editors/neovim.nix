3fe99df5aa15bea31920abbf05341b59eb7b9e70
Evie Ciobanu <me@evie.ro>
2025-09-05 18:09:32 +0300

    neovim: add colorful-menu

--- modules/home-manager/editors/neovim.nix
+++ modules/home-manager/editors/neovim.nix
@@ -450,6 +450,21 @@ in
           enable = true;
           autoEnableSources = true;
           settings = {
+            formatting.format.__raw = ''
+              function(entry, vim_item)
+                local highlights_info = require("colorful-menu").cmp_highlights(entry)
+
+                -- highlight_info is nil means we are missing the ts parser, it's
+                -- better to fallback to use default `vim_item.abbr`. What this plugin
+                -- offers is two fields: `vim_item.abbr_hl_group` and `vim_item.abbr`.
+                if highlights_info ~= nil then
+                    vim_item.abbr_hl_group = highlights_info.highlights
+                    vim_item.abbr = highlights_info.text
+                end
+
+                return vim_item
+              end
+            '';
             sources = [
               { name = "async_path"; }
               { name = "cmp-dictionary"; }
@@ -982,6 +997,12 @@ in
           settings = {
             highlight_visible = true;
           };
+        };
+
+        # Color completion menus.
+        # TODO: use it with blink https://github.com/xzbdmw/colorful-menu.nvim
+        colorful-menu = {
+          enable = true;
         };
       };
     };
