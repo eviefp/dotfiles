3064ad189db8d690d42b7448138f09fecd99822e
Evie Ciobanu <me@evie.ro>
2025-09-08 01:08:10 +0300

    neovim: better lean support

--- modules/home-manager/editors/neovim.nix
+++ modules/home-manager/editors/neovim.nix
@@ -1120,6 +1120,44 @@ in
 
         # Hilight code in nix strings. Auto-detect plus `/* lang */` hints before the string.
         hmts.enable = true;
+
+        # lean lsp
+        lean = {
+          enable = true;
+          settings = {
+            mappings = true;
+            lsp = {
+              init_options = {
+                edit_delay = 0;
+                hasWidgets = true;
+              };
+            };
+
+            abbreviations = {
+              enable = true;
+              extra = {
+                # Add a \wknight abbreviation to insert ♘
+                wknight = "♘";
+              };
+              leader = "\\";
+            };
+
+            infoview = {
+              autoopen = false;
+            };
+
+            progress_bars = {
+              enable = true;
+              character = "|";
+              priority = 10;
+            };
+
+            stderr = {
+              enable = true;
+              height = 5;
+            };
+          };
+        };
       };
     };
   };
