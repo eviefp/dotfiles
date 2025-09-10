54488e1e79bb3dc049e6032207bc40afcbd73c93
Evie Ciobanu <me@evie.ro>
2025-09-10 20:03:28 +0300

    neovim: switch to cyberdream

--- modules/home-manager/editors/neovim.nix
+++ modules/home-manager/editors/neovim.nix
@@ -346,49 +346,60 @@ in
         }
       '';
 
-      colorschemes.vscode = {
+      # TODO: move these into some default system colors.
+      #
+      # M.default = {
+      #     bg = "#16181a",
+      #     bg_alt = "#1e2124",
+      #     bg_highlight = "#3c4048",
+      #     fg = "#ffffff",
+      #     grey = "#7b8496",
+      #     blue = "#5ea1ff",
+      #     green = "#5eff6c",
+      #     cyan = "#5ef1ff",
+      #     red = "#ff6e5e",
+      #     yellow = "#f1ff5e",
+      #     magenta = "#ff5ef1",
+      #     pink = "#ff5ea0",
+      #     orange = "#ffbd5e",
+      #     purple = "#bd5eff",
+      # }
+      #
+      # M.light = {
+      #     bg = "#ffffff",
+      #     bg_alt = "#eaeaea",
+      #     bg_highlight = "#acacac",
+      #     fg = "#16181a",
+      #     grey = "#7b8496",
+      #     blue = "#0057d1",
+      #     green = "#008b0c",
+      #     cyan = "#008c99",
+      #     red = "#d11500",
+      #     yellow = "#997b00",
+      #     magenta = "#d100bf",
+      #     pink = "#f40064",
+      #     orange = "#d17c00",
+      #     purple = "#a018ff",
+      # }
+      colorschemes.cyberdream = {
         enable = true;
         settings = {
+          variant = "dark";
           transparent = true;
-          italic_commnts = true;
-          underline_links = true;
-          disable_nvimtree_bg = true;
-          group_overrides = {
-            StatusLine = {
-              bg = "NONE";
+          saturation = 1;
+          italic_comments = true;
+          borderless_pickers = false;
+          terminal_colors = true;
+          highlights = {
+            Comment = {
+              fg = "pink";
+              italic = true;
             };
-            MoreMsg = {
-              bg = "NONE";
+            Character = {
+              fg = "orange";
             };
-            TabLine = {
-              bg = "NONE";
-            };
-            TabLineSel = {
-              bg = "NONE";
-              underline = true;
-              sp = "NvimLightYellow";
-            };
-            TabLineFill = {
-              bg = "NONE";
-            };
-            PMenu = {
-              bg = "NONE";
-            };
-            PMenuSel = {
-              bg = "NONE";
-              underline = true;
-              sp = "NvimLightYellow";
-            };
-            "@function" = {
-              fg = "#9c74ef";
-            };
-            BufferCurrent = {
-              sp = "NvimLightYellow";
-              underline = true;
-            };
-            GitSignsCurrentLineBlame = {
-              bg = "NONE";
-              fg = "#4EC9B0";
+            String = {
+              fg = "orange";
             };
           };
         };
@@ -1309,6 +1320,15 @@ in
         # highlights FIX, TODO, HACK, WARN, PERF, NOTE, TEST. can be configured.
         todo-comments = {
           enable = true;
+        };
+
+        # enforce transparency
+        transparent = {
+          enable = true;
+          settings = {
+            extra_groups = [ ];
+            exclude_grups = [ ];
+          };
         };
 
       };
