9bea1868ab6fb2221fc618d129cf7165877becdc
Evie Ciobanu <me@evie.ro>
2025-09-11 15:21:55 +0300

    neovim: add visual multi cursors

--- modules/home-manager/editors/neovim.nix
+++ modules/home-manager/editors/neovim.nix
@@ -1433,6 +1433,17 @@ in
           enable = true;
         };
 
+        # visual multi cursors
+        visual-multi = {
+          enable = true;
+          settings = {
+            mouse_mappings = 0;
+            silent_exit = 1;
+            show_warnings = 1;
+            default_mappings = 1;
+          };
+        };
+
       };
     };
   };
