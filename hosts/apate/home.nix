49b570ac9aad844b4c01c9a565bc7e545085daca
Evie Ciobanu <me@evie.ro>
2025-09-18 12:28:27 +0300

    home: oh?

--- hosts/apate/home.nix
+++ hosts/apate/home.nix
@@ -4,32 +4,32 @@
     common
   ];
 
-  config.evie = {
-    common.enable = true;
-
-    dev = {
-      haskell.enable = true;
-      lua.enable = true;
-      provers.enable = true;
-    };
-
-    editors = {
-      emacs.enable = false;
-      neovim.obsidian = true;
-    };
-    system.gpg.enable = true;
-    programs.firefox.enable = true;
-    term.kitty.enable = true;
-
-    # TODO: aerospace
-  };
-
   config = {
     home = {
       stateVersion = "25.05";
       sessionVariables = {
         NIX_PATH = "nixpkgs=${dotfiles.nixpkgs}";
       };
+    };
+
+    evie = {
+      common.enable = true;
+
+      dev = {
+        haskell.enable = true;
+        lua.enable = true;
+        provers.enable = true;
+      };
+
+      editors = {
+        emacs.enable = false;
+        neovim.obsidian = true;
+      };
+      system.gpg.enable = true;
+      programs.firefox.enable = true;
+      term.kitty.enable = true;
+
+      # TODO: aerospace
     };
   };
 }
