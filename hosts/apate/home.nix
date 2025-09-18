74a650c955fcb193266acc42b368f4968baaea68
Evie Ciobanu <me@evie.ro>
2025-09-18 12:29:12 +0300

    home: maybe this, then?

--- hosts/apate/home.nix
+++ hosts/apate/home.nix
@@ -4,32 +4,30 @@
     common
   ];
 
-  config = {
-    home = {
-      stateVersion = "25.05";
-      sessionVariables = {
-        NIX_PATH = "nixpkgs=${dotfiles.nixpkgs}";
-      };
+  home = {
+    stateVersion = "25.05";
+    sessionVariables = {
+      NIX_PATH = "nixpkgs=${dotfiles.nixpkgs}";
+    };
+  };
+
+  config.evie = {
+    common.enable = true;
+
+    dev = {
+      haskell.enable = true;
+      lua.enable = true;
+      provers.enable = true;
     };
 
-    evie = {
-      common.enable = true;
-
-      dev = {
-        haskell.enable = true;
-        lua.enable = true;
-        provers.enable = true;
-      };
-
-      editors = {
-        emacs.enable = false;
-        neovim.obsidian = true;
-      };
-      system.gpg.enable = true;
-      programs.firefox.enable = true;
-      term.kitty.enable = true;
-
-      # TODO: aerospace
+    editors = {
+      emacs.enable = false;
+      neovim.obsidian = true;
     };
+    system.gpg.enable = true;
+    programs.firefox.enable = true;
+    term.kitty.enable = true;
+
+    # TODO: aerospace
   };
 }
