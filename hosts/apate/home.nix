f59b989b95cd9b3c1f4efdc17c9fe6378355a34a
Evie Ciobanu <me@evie.ro>
2025-09-18 12:31:28 +0300

    Revert "home: maybe this, then?"
    This reverts commit 74a650c955fcb193266acc42b368f4968baaea68.

--- hosts/apate/home.nix
+++ hosts/apate/home.nix
@@ -4,30 +4,32 @@
     common
   ];
 
-  home = {
-    stateVersion = "25.05";
-    sessionVariables = {
-      NIX_PATH = "nixpkgs=${dotfiles.nixpkgs}";
-    };
-  };
-
-  config.evie = {
-    common.enable = true;
-
-    dev = {
-      haskell.enable = true;
-      lua.enable = true;
-      provers.enable = true;
+  config = {
+    home = {
+      stateVersion = "25.05";
+      sessionVariables = {
+        NIX_PATH = "nixpkgs=${dotfiles.nixpkgs}";
+      };
     };
 
-    editors = {
-      emacs.enable = false;
-      neovim.obsidian = true;
-    };
-    system.gpg.enable = true;
-    programs.firefox.enable = true;
-    term.kitty.enable = true;
+    evie = {
+      common.enable = true;
 
-    # TODO: aerospace
+      dev = {
+        haskell.enable = true;
+        lua.enable = true;
+        provers.enable = true;
+      };
+
+      editors = {
+        emacs.enable = false;
+        neovim.obsidian = true;
+      };
+      system.gpg.enable = true;
+      programs.firefox.enable = true;
+      term.kitty.enable = true;
+
+      # TODO: aerospace
+    };
   };
 }
