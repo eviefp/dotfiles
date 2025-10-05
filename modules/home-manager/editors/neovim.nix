0efebcd0b2d5045d36e899fcf97e9d39ae7d8fd2
Evie Ciobanu <me@evie.ro>
2025-10-05 22:58:14 +0300

    neovim: add presenterm.nvim

--- modules/home-manager/editors/neovim.nix
+++ modules/home-manager/editors/neovim.nix
@@ -418,6 +418,20 @@ in {
             hash = "sha256-8KRWE0eNPSBh4HT6sDix9hz6QWcVk3Ne1bjmb/NDPio=";
           };
         }) # https://github.com/y3owk1n/time-machine.nvim
+        (pkgs.vimUtils.buildVimPlugin {
+          pname = "presenterm.nvim";
+          version = "v1.4.0";
+          src = pkgs.fetchFromGitHub {
+            owner = "Piotr1215";
+            repo = "presenterm.nvim";
+            rev = "9918cf87094be527af97ea5a8bd69f7c30c62a3e";
+            hash = "sha256-fpBj2dO437LW+cdWs8wg2GYJoZer5XBWq9BL4e2R/OM=";
+          };
+          patches = [
+            # https://github.com/Piotr1215/presenterm.nvim/pull/4
+            ./presenterm-nvim-use-slide-marker.patch
+          ];
+        }) # https://github.com/Piotr1215/presenterm.nvim
       ];
 
       extraConfigLua = ''
@@ -437,7 +451,20 @@ in {
         })
 
         require('time-machine').setup {
-        };
+        }
+
+        require('presenterm').setup {
+          slide_marker = '---',
+          default_keybindings = true,
+          picker = {
+            provider = 'telescope',
+          },
+          preview = {
+            command = 'presenterm',
+            presentation_preview_sync = true,
+            login_shell = false,
+          },
+        }
       '';
 
       colorschemes.cyberdream = {
