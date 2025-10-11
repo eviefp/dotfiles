83b911edee388dacff818ae7f9ff34006f85ab12
Evie Ciobanu <me@evie.ro>
2025-10-11 18:19:56 +0300

    module: lualine

--- modules/home-manager/editors/neovim/default.nix
+++ modules/home-manager/editors/neovim/default.nix
@@ -23,6 +23,7 @@ in {
     # Core
     ./colorscheme.nix
     ./keybindings.nix
+    ./lualine.nix
     ./treesitter.nix
 
     # Text editing
@@ -227,67 +228,6 @@ in {
 
       plugins = {
         ## UI
-
-        lualine = {
-          enable = true;
-
-          settings = {
-            options = {
-              theme = "palenight";
-            };
-            winbar = {
-              lualine_a = [
-                {
-                  __unkeyed-1 = "filename";
-                  path = 1;
-                  shortening_target = 0;
-                }
-              ];
-              lualine_b = [];
-              lualine_c = [
-              ];
-              lualine_x = [];
-              lualine_y = [];
-              lualine_z = [];
-            };
-            inactive_winbar = {
-              lualine_a = [
-                {
-                  __unkeyed-1 = "filename";
-                  path = 1;
-                  shortening_target = 0;
-                }
-              ];
-              lualine_b = [];
-              lualine_c = [
-              ];
-              lualine_x = [];
-              lualine_y = [];
-              lualine_z = [];
-            };
-            sections = {
-              lualine_a = ["mode" "hostname"];
-              lualine_b = ["branch" "diff" "diagnostics"];
-              lualine_c = ["filename"];
-              lualine_x = [];
-              lualine_y = [
-                "encoding"
-                "fileformat"
-              ];
-              lualine_z = ["location"];
-            };
-            inactive_sections = {
-              lualine_a = [];
-              lualine_b = [];
-              lualine_c = ["filename"];
-              lualine_x = ["location"];
-              lualine_y = [];
-              lualine_z = [];
-            };
-            extensions = ["quickfix"];
-          };
-        };
-
         telescope = {
           enable = true;
 
---
+++ modules/home-manager/editors/neovim/lualine.nix
@@ -0,0 +1,71 @@
+{
+  lib,
+  config,
+  ...
+}: let
+  cfg = config.evie.editors.neovim;
+in {
+  config = lib.mkIf cfg.enable {
+    programs.nixvim = {
+      plugins.lualine = {
+        enable = true;
+
+        settings = {
+          options = {
+            theme = "auto";
+          };
+          winbar = {
+            lualine_a = [
+              {
+                __unkeyed-1 = "filename";
+                path = 1;
+                shortening_target = 0;
+              }
+            ];
+            lualine_b = [];
+            lualine_c = [
+            ];
+            lualine_x = [];
+            lualine_y = [];
+            lualine_z = [];
+          };
+          inactive_winbar = {
+            lualine_a = [
+              {
+                __unkeyed-1 = "filename";
+                path = 1;
+                shortening_target = 0;
+              }
+            ];
+            lualine_b = [];
+            lualine_c = [
+            ];
+            lualine_x = [];
+            lualine_y = [];
+            lualine_z = [];
+          };
+          sections = {
+            lualine_a = ["mode" "hostname"];
+            lualine_b = ["branch" "diff" "diagnostics"];
+            lualine_c = ["filename"];
+            lualine_x = [];
+            lualine_y = [
+              "encoding"
+              "fileformat"
+            ];
+            lualine_z = ["location"];
+          };
+          inactive_sections = {
+            lualine_a = [];
+            lualine_b = [];
+            lualine_c = ["filename"];
+            lualine_x = ["location"];
+            lualine_y = [];
+            lualine_z = [];
+          };
+          extensions = ["quickfix"];
+        };
+      };
+    };
+  };
+}
