ad7fc64dc0bf66453be178743b44d7b9e79af33d
Evie Ciobanu <me@evie.ro>
2025-10-11 14:57:25 +0300

    module: time-machine

--- modules/home-manager/editors/neovim/default.nix
+++ modules/home-manager/editors/neovim/default.nix
@@ -20,6 +20,7 @@ in {
 
     # Text editing
     ./tabular.nix
+    ./time-machine.nix
 
     # Git related
     ./blame.nix
@@ -365,40 +366,9 @@ in {
           action = ":lua require('origami').l()<cr>";
           options.desc = "un-fold region";
         }
-        # time-machine
-        {
-          key = "<leader>tm";
-          action = "<cmd>TimeMachineToggle<cr>";
-          options.desc = "Time Machine: toggle tree";
-        }
-        {
-          key = "<leader>tx";
-          action = "<cmd>TimeMachinePurgeCurrent<cr>";
-          options.desc = "Time Machine: purge current";
-        }
-        {
-          key = "<leader>tX";
-          action = "<cmd>TimeMachinePurgeAll<cr>";
-          options.desc = "Time Machine: purge ALL";
-        }
-        {
-          key = "<leader>tl";
-          action = "<cmd>TimeMachineLogShow<cr>";
-          options.desc = "Time Machine: show logs";
-        }
       ];
 
       extraPlugins = [
-        (pkgs.vimUtils.buildVimPlugin {
-          pname = "time-machine.nvim";
-          version = "v1.5.4";
-          src = pkgs.fetchFromGitHub {
-            owner = "y3owk1n";
-            repo = "time-machine.nvim";
-            rev = "6657a7db8a07ecfa8a266be34bebfb6827bb2eab";
-            hash = "sha256-8KRWE0eNPSBh4HT6sDix9hz6QWcVk3Ne1bjmb/NDPio=";
-          };
-        }) # https://github.com/y3owk1n/time-machine.nvim
         (pkgs.vimUtils.buildVimPlugin {
           pname = "presenterm.nvim";
           version = "v1.4.0";
