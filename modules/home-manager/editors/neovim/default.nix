707b7c6e8ab649b08b134f855c6b25248e725262
Evie Ciobanu <me@evie.ro>
2025-10-11 23:28:07 +0300

    module: obsidian

--- modules/home-manager/editors/neovim/default.nix
+++ modules/home-manager/editors/neovim/default.nix
@@ -40,12 +40,10 @@ in {
     # Text editing / viewing
     ./blink-cmp.nix
     ./ccc.nix
-    ./diagram.nix
     ./indent-o-matic.nix
     ./grug-far.nix
     ./gx.nix
     ./markview.nix
-    ./presenterm.nix
     ./surround.nix
     ./tabular.nix
     ./time-machine.nix
@@ -77,6 +75,9 @@ in {
     ./lang/typescript.nix
 
     # Utilities / misc
+    ./diagram.nix
+    ./obsidian.nix
+    ./presenterm.nix
     ./sops.nix
   ];
 
@@ -87,10 +88,6 @@ in {
 
   config = lib.mkIf cfg.enable {
     home.packages = [
-      pkgs.obsidian
-
-      # grug-far
-
       # snacks
       pkgs.ghostscript
     ];
@@ -121,46 +118,6 @@ in {
 
       keymaps = [
         # Obsidian
-        {
-          key = "<leader>fn";
-          action = ":Obsidian quick_switch<cr>";
-          options.desc = "obsidian open";
-        }
-        {
-          key = "<leader>ot";
-          action = ":Obsidian tags<cr>";
-          options.desc = "obsidian tags";
-        }
-        {
-          key = "<leader>o1";
-          action = ":Obsidian today<cr>";
-          options.desc = "obsidian today";
-        }
-        {
-          key = "<leader>o2";
-          action = ":Obsidian tomorrow<cr>";
-          options.desc = "obsidian tomorrow";
-        }
-        {
-          key = "<leader>ol";
-          action = ":Obsidian link<cr>";
-          options.desc = "obsidian link";
-        }
-        {
-          key = "<leader>oL";
-          action = ":Obsidian link_new<cr>";
-          options.desc = "obsidian link create";
-        }
-        {
-          key = "<leader>or";
-          action = ":Obsidian rename";
-          options.desc = "obsidian rename";
-        }
-        {
-          key = "<leader>ox";
-          action = ":Obsidian extract_note<cr>";
-          options.desc = "obsidian extract note";
-        }
         # yazi
         {
           key = "<leader>ya";
@@ -184,51 +141,6 @@ in {
         #######################################################################
         # Experiments
         #######################################################################
-        obsidian = lib.mkIf cfg.obsidian {
-          enable = true;
-          settings = {
-            legacy_commands = false;
-
-            workspaces = [
-              {
-                name = "notes";
-                path = "~/code/notes";
-              }
-            ];
-
-            notes_subdir = "notes";
-
-            daily_notes = {
-              folder = "notes/daily";
-              date_format = "%Y-%m-%d";
-              alias_format = "%B %-d, %T";
-              workdays_only = false;
-            };
-
-            completion = {
-              blink = true;
-              min_chars = 2;
-            };
-
-            new_notes_location = "notes_subdir";
-
-            preferred_link_style = "wiki";
-
-            picker = {
-              name = "telescope.nvim";
-            };
-
-            ui = {
-              enable = true;
-              ignore_conceal_warn = true;
-            };
-
-            checkbox = {
-              order = [" " ">" "x" "~"];
-            };
-          };
-        };
-
         # edit files in a buffer
         oil = {
           enable = true;
