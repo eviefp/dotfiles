e31d867514d7cf2221683f9c09d8cb5d17da1915
Evie Ciobanu <me@evie.ro>
2025-10-11 23:10:15 +0300

    module: noice

--- modules/home-manager/editors/neovim/default.nix
+++ modules/home-manager/editors/neovim/default.nix
@@ -29,6 +29,7 @@ in {
     ./hlchunk.nix
     ./lualine.nix
     ./marks.nix
+    ./noice.nix
     ./telescope.nix
     ./web-devicons.nix
 
@@ -182,56 +183,6 @@ in {
         #######################################################################
         # Experiments
         #######################################################################
-
-        # UI improvements
-        noice = {
-          enable = true;
-          settings = {
-            cmdline = {
-              enabled = true;
-              view = "cmdline_popup";
-            };
-            messages = {
-              enabled = true;
-              view_error = "notify";
-              view_warn = "notify";
-              view_history = "messages";
-              view_search = "virtualtext";
-            };
-            popupmenu = {
-              enabled = true;
-              backend = "nui";
-            };
-            notify = {
-              enabled = true;
-              view = "notify";
-            };
-            lsp = {
-              enabled = false;
-            };
-            presets = {
-              bottom_search = true;
-              command_palette = true;
-              long_message_to_split = true;
-              inc_rename = false;
-              lsp_doc_border = true;
-            };
-          };
-        };
-
-        # Used by noice.
-        nui.enable = true;
-
-        # Used by noice.
-        notify = {
-          enable = true;
-          settings = {
-            level = "warn";
-            background_colour = "#000000";
-            max_width = 30;
-          };
-        };
-
         # surround; ys/yS
         nvim-surround = {
           enable = true;
---
+++ modules/home-manager/editors/neovim/noice.nix
@@ -0,0 +1,53 @@
+{...}: {
+  config = {
+    programs.nixvim = {
+      plugins = {
+        noice = {
+          enable = true;
+          settings = {
+            cmdline = {
+              enabled = true;
+              view = "cmdline_popup";
+            };
+            messages = {
+              enabled = true;
+              view_error = "notify";
+              view_warn = "notify";
+              view_history = "messages";
+              view_search = "virtualtext";
+            };
+            popupmenu = {
+              enabled = true;
+              backend = "nui";
+            };
+            notify = {
+              enabled = true;
+              view = "notify";
+            };
+            lsp = {
+              enabled = false;
+            };
+            presets = {
+              bottom_search = true;
+              command_palette = true;
+              long_message_to_split = true;
+              inc_rename = false;
+              lsp_doc_border = true;
+            };
+          };
+        };
+
+        nui.enable = true;
+
+        notify = {
+          enable = true;
+          settings = {
+            level = "warn";
+            background_colour = "#000000";
+            max_width = 30;
+          };
+        };
+      };
+    };
+  };
+}
